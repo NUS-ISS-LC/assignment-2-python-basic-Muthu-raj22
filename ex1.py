f925f37ec70640922157800db63e514bc1cbe750
Muthu-raj22 <e1539335@u.nus.edu>
2025-11-10 01:54:53 +0000

    assignment2

--- ex1.py
+++ ex1.py
@@ -1,3 +1,10 @@
 def find(s, n):
-# write your implementation here
-    return None
+
+    for i in range(len(s)):
+        for j in range(i+1,len(s)):
+            if(s[i]+s[j]==n):
+                return [i,j]
+
+nums = [3,2,4]
+target = 5
+print(find(nums, target))
